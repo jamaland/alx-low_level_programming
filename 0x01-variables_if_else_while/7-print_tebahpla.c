e2c312f6321a4e195c4856b1dc7324ae6e4842a6
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 19:31:06 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n", q[] = "q", e[] = "e";
+	int i = 27;
+
+	while (i > 0)
+	{
+		if (alpha[i] != q[0] && alpha[i] != e[0])
+		putchar(alpha[i]);
+		i--
+	}
+	putchar('\n');
+	return (0);
+}
