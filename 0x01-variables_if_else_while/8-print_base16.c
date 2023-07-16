b04097c37a8b3830026402dea063c8c85f5fcda3
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 20:03:06 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 15)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
