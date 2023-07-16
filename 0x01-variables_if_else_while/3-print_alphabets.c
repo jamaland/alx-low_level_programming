52eded5062480833215fc219cfee3bbc273217c1
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 18:49:55 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	int i = 0;
+
+	while (i < 26*2+1)
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+	return (0);
+}
