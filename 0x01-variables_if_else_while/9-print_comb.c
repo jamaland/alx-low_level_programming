f7d4a619a8f112c67d5f1f4371ab085a5278ffd8
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 20:08:14 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(48 + i + ', ');
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
