84ed8252819f2a44813dc9749a91a77df31eeeb8
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 18:11:12 -0700

    1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, first, last;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+    first = n;
+    last = n %10;
+    while(first >= 10)
+    {
+        first = first / 10;
+    }
+    if( last == 0)
+        printf("Last digit of %d is 0 and is 0", n);
+    else if( first<last)
+        printf("Last digit of %d is %d and is greater than %d", n, last, first);
+    else if( first<last)
+        printf("Last digit of %d is %d and is less than %d and not 0", n, last, first);
+	return (0);
+}
