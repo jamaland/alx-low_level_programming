79f45de194f8fdc620a02534984b3b3a0f0b4a8d
Jamal Maarad <j.maarad49@gmail.com>
2023-07-15 18:42:31 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha[]="abcdefghijklmnopqrstuvwxyz";
+
+	for(int i=0;i<6;i++)
+		putchar(alpha[i]);
+	return (0);
+}
