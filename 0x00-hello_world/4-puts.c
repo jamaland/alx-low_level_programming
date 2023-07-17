41da9aecf0894ec0c7079147a3574b66d67ddf71
Jamal Maarad <j.maarad49@gmail.com>
2023-07-17 10:37:18 -0700

    4.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
