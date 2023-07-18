198bdb263cdf74014fb9dd729f59006daa83ead1
Jamal Maarad <j.maarad49@gmail.com>
2023-07-18 09:42:57 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: 'This program print alphabet using an external function '
+ * Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int i, j;
+	
+	for (j=0; j < 10; j++)
+	{
+		i = 0;
+		while (i < 26)
+		{
+			_putchar(i + 97);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
