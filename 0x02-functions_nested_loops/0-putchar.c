e92d5403404acb30ae8658422bdb3276e303451f
Jamal Maarad <j.maarad49@gmail.com>
2023-07-18 09:17:48 -0700

    0.putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,13 @@
  * Description: 'this is a c program'
  * Return: Always 0 (Success)
 */
-int main(void){
+int main(void)
+{
 	char str[] = "_putchar";
 	int i = 0;
 
 	while (str[i] != '\0')
-	{
-		
+	{	
 		_putchar(str[i]);
 		i++;
 	}
