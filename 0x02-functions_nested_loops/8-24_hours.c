d840f593ed0445ae1380db2fd4a0d8fc30639581
Jamal Maarad <j.maarad49@gmail.com>
2023-07-18 17:38:26 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <time.h>
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Description: 'This program show minutes '
+ * Return: Always void (Success)
+*/
+void jack_bauer(void)
+{
+	int i = 0, j;
+
+	while (i < 24)
+	{
+		j = 0;
+		while ( j < 60)
+		{
+			if (i < 10)
+				_putchar(48);
+			_putchar('0' + i);
+			_putchar(':');
+			if (j < 10)
+                                _putchar(48);
+                        _putchar('0' + j);
+			if (i != 24 && j != 59)
+				_putchar('\n');
+		j++;
+		}
+		i++;
+	}	
+}
