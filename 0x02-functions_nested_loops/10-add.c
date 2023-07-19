4f9e804834d44775a05ad9894d602b922caf94cd
Jamal Maarad <j.maarad49@gmail.com>
2023-07-18 18:30:11 -0700

    10. a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a : a variable
+ * @b : variable
+ * Description: 'this is a c program'
+ * Return: Always 0 (Success)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
