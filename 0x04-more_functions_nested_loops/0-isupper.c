3d8d2f41c50e343cd98625f91446a361cee63961
Jamal Maarad <j.maarad49@gmail.com>
2023-07-20 17:51:42 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+/**
+ * isupper - func
+ * Description: 'the program's verify a number type'
+ * @c: variable character
+ * Return: 1 (true) 0(false)
+*/
+int isupper(char c)
+{
+		return (isupper(c));
+}
