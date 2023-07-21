c80951cf91360cf85c143b95e7bac0871f2b47ce
Jamal Maarad <j.maarad49@gmail.com>
2023-07-21 05:05:29 -0700

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void _putchar(int);
 int _isupper(int);
 int _isdigit(int);
@@ -5,3 +7,4 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+#endif /* MAIN_H */
