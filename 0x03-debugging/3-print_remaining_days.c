7c1f8b1a2897cc793045c36b0347f8d93107e704
Jamal Maarad <j.maarad49@gmail.com>
2023-07-20 17:47:44 -0700

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,6 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
  * Description: function to verify a date
  * @month: month in number format
  * @day: day of month
