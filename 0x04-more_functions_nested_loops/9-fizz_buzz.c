b2d881344d8c036314d5fe566d26477704454e1a
Jamal Maarad <j.maarad49@gmail.com>
2023-07-21 09:35:26 -0700

    9.Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: 'This program print numbers using an _putchar '
  * Return: 0
 */
+
 int main(void)
 {
 	int i = 1;
@@ -17,10 +19,9 @@ int main(void)
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else
-			printf("%d",i);
+			printf("%d", i);
 		if (i != 100)
 			printf(" ");
-			
 		i++;
 	}
 	printf("\n");
