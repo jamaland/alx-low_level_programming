b4f7b6cf40796b1257806bd57ff11e00d72b74b5
Jamal Maarad <j.maarad49@gmail.com>
2023-07-21 06:58:57 -0700

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ * Description: 'This program print numbers using an _putchar '
+ * @n: integer
+ * Return: Void
+*/
+void print_square(int n)
+{
+	int i = 0, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
