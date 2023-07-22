dcd3945eaee67e2a4c77e4eaeccb8408524d7b64
Jamal Maarad <j.maarad49@gmail.com>
2023-07-21 17:35:25 -0700

    3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,12 +7,14 @@
 
 int main(void)
 {
-	int month = 02, day = 60, year = 2000;
+	int month = 02, day = 60, year = 2000, i;
 
 	if (year % 4 == 0 && (year % 400 == 0 || !(year % 100 == 0)))
 	{
 		if (month == 2 && day > 60)
 		{
+			for (i = 0; i < 5; i++)
+				printf("___\n");
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
