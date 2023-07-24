b9b8e3263eeb4b99811ea3260b50aab2732268f7
Jamal Maarad <j.maarad49@gmail.com>
2023-07-24 10:35:22 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
  * reset_to_98 - Entry point
  * Description: 'This program takes a pointer to an int as parameter
  * and updates the value it points to to 98 '
+ * @n: Pointer of Integer
  * Return: Always void
 */
 void reset_to_98(int *n)
