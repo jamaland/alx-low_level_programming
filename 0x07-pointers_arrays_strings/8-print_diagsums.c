077602a6fb6a669bd9dfd5c293f1bd53c9b1e323
Jamal Maarad <j.maarad49@gmail.com>
2023-08-01 17:49:31 -0700

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,9 +12,8 @@ void print_diagsums(int *a, int size)
 	int i, sum = 0, sum_rev = 0;
 
 	for (i = 0; i < size; i++)
-			sum += a[i][i];
+		sum += a[i][i];
 	for (i = 0; i < size; i++)
-                        sum_rev += a[i][size - i -1];
-
+		sum_rev += a[i][size - i - 1];
 	printf("%d, %d\n", sum, sum_rev);
 }
