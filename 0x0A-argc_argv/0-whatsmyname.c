5c9235ac005b74031a242144f7326c81a7bf66cb
Jamal Maarad <j.maarad49@gmail.com>
2023-08-04 07:46:27 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 
 /**
  * main - check the code
- *
+ * @argc: size of arguments array
+ * @argv: arry of params
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	if (argc >0)
+	if (argc > 0)
 		printf("%s\n", argv[0]);
 	return (0);
 }
