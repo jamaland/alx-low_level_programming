2cd3845c64aad86123b887b34e84c0073bbfd5d1
Jamal Maarad <j.maarad49@gmail.com>
2023-08-05 06:42:33 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,16 +15,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			num = atoi(argv[i]);
+			num = atoi(argv[i];
 			if (isdigit(*argv[i]) == 0)
 			{
 				printf("Error\n");
-                                return (1);
+				return (1);
 			}
 			else
 			{
-				printf("num: %d\n",num);
-                                sum += num;
+				sum += num;
 			}
 		}
 	}
