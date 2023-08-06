d9c3bcbc062e745b69b4528d5eeb2e40efe6e31b
Jamal Maarad <j.maarad49@gmail.com>
2023-08-05 18:29:52 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 /**
  * _atoi - Entry point
  * Description: 'This program convert a string to an integer.'
@@ -10,22 +11,5 @@
 */
 int _atoi(char *s)
 {
-	int i, l = strlen(s), num, n = 0, mul = 1;
-
-	for (i = 0; i < l;  i++)
-	{
-		printf("s : %s, ", s);
-		if (isdigit(s))
-		{
-			num = (int)s;
-			n += (num * mul);
-			mul *= 10;
-		}
-	       	else if (s == '-')
-		{
-			n *= -1;
-		}
-		s++;
-	}
 	return (1);
 }
