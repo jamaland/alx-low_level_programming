7458a4691e28c5980eac8fa6d5e8663195d8e18a
Jamal Maarad <j.maarad49@gmail.com>
2023-08-08 18:35:10 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
  * str_concat - Entry point
  * Description: 'This program concat two strings'
@@ -12,23 +10,27 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	char *cp_str;
+	char *cp_str, *tmp, *tmp2;
 	int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	tmp = s1;
 	while (*s1)
 	{
 		size1++;
 		s1++;
 	}
+	s1 = tmp;
+	tmp2 = s2;
 	while (*s2)
 	{
 		size2++;
 		s2++;
 	}
+	s2 = tmp2;
 	cp_str = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (cp_str == NULL)
 	{
@@ -36,14 +38,14 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
-		for (i = 0; i < size1; i++)
-			cp_str[i] = *(s1 - size1 + i);
-		printf("i>%s", (s2 + i));
-		for (; i < size1 + size2; i++)
-			cp_str[i] = *(s2 - size2 + i);
+		for (i = 0; i < size1 + size2; i++)
+		{
+			if (i < size1)
+				cp_str[i] = *(s1 + i);
+			else
+				cp_str[i] = *(s2 + i - size1);
+		}
 		cp_str[i] = '\0';
-		if (cp_str == NULL)
-			return (NULL);
 		return (cp_str);
 	}
 }
