32fd91f57adcdca37c5fed042a4c44e8abcb6bda
Jamal Maarad <j.maarad49@gmail.com>
2023-08-09 16:15:41 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Entry point
+ * Description: 'This program return a matrix'
+ * @width: Integer 
+ * @height: Integer
+ * Return: Integer
+*/
+int **alloc_grid(int width, int height)
+{
+	int **matrix, i, j;
+
+	if (width <= 0 || height <=0)
+		return (NULL);
+
+	matrix = (int**)malloc(width * sizeof(int*));
+	for (i = 0; i < width; i++)
+		matrix[i] = (int*)malloc(height * sizeof(int*));
+	if (matrix == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < width * height; i++)
+			for (j = 0; j < heigth; j++)
+				matrix[i][j] = 0;
+		return (matrix);
+	}
+}
