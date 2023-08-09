9936e174107d78bb1258ae044a8cb9a18f3f370b
Jamal Maarad <j.maarad49@gmail.com>
2023-08-09 16:36:20 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		matrix[i] = (int *)malloc(width * sizeof(int *));
+		matrix[i] = (int *)malloc(width * sizeof(int));
 		if (matrix[i] == NULL)
 			return (NULL);
 	}
