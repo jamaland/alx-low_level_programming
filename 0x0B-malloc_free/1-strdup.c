3257f10c6499c6bdea2a6e9b78690a8a8f0f5bf8
Jamal Maarad <j.maarad49@gmail.com>
2023-08-08 17:23:01 -0700

    Resolve size of allocation memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,6 +13,8 @@ char *_strdup(char *str)
 	char *cp_str;
 	int size = 0, i;
 
+	if (str == NULL)
+               return (NULL);
 	while (*str)
 	{
 		size++;
@@ -20,8 +22,6 @@ char *_strdup(char *str)
 	}
 	str = str - size;
 	cp_str = malloc(sizeof(char) * (size + 1));
-	if (str == NULL)
-	       return (NULL);
 	if (cp_str == NULL)
 	{
 		return (NULL);
