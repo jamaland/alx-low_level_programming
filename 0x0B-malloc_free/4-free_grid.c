f569ffd14cbd85f537170d05a0ab04763188f5c2
Jamal Maarad <j.maarad49@gmail.com>
2023-08-09 17:07:08 -0700

    4. It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - Entry point
+ * Description: 'This program free a matrix'
+ * @grid: array of Integer
+ * @height: Integer
+ * Return: Void
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return ;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
