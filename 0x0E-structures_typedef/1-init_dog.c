0fd57a7ae3ce42d752367bfd82623adcf0997a63
Jamal Maarad <j.maarad49@gmail.com>
2023-08-14 16:35:17 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * init_dog - Init entity dog
+ * Description: 'This program initialize a entity dog'
+ * @d: Pointer of entity dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ * Return: Always Void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *, char *, float, char *);
 #endif /*DOG_H*/
