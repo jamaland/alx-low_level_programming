dec76640d8098e877fe921edb46318d4beb56604
Jamal Maarad <j.maarad49@gmail.com>
2023-08-17 05:13:51 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function
+ * Description: 'This program prints a name'
+ * @name: Param of type char
+ * @f: Param of type func
+ * Return: Always void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef F_POINTER
+#define F_POINTER
+void print_name(char *, void (*)(char *));
+#endif
