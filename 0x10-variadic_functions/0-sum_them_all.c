08d2ced83329b935b9a28475f4f6de003d68810f
Jamal Maarad <j.maarad49@gmail.com>
2023-08-17 17:29:48 -0700

    betty alignement

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start (ap, n); 
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-	
-	va_end (ap);
+
+	va_end(ap);
 	return (sum);
 }
