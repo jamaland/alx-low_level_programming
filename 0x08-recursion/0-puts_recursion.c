acebeebcfc45974654dbdf4dc7a641f692b9c7bf
Jamal Maarad <j.maarad49@gmail.com>
2023-08-20 09:08:37 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * _puts_recursion - Function
  * Description: 'This program prints a string recursevly'
@@ -10,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		puts(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-		puts('\n');
+		_putchar('\n');
 }
