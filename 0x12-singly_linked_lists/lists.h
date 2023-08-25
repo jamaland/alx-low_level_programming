09a06989bf3e9d86d085d0b27e6eb706149793ab
Jamal Maarad <j.maarad49@gmail.com>
2023-08-24 18:19:40 -0700

    0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,16 @@
 #ifndef H_LISTS
 #define H_LISTS
+/**
+ * struct list_s - LinkedList
+ * @str: string
+ * @len: string length
+ * @next: next node
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
