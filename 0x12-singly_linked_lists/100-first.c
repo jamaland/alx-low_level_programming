1228f097c68b799102354f8efa1b9748e3c0ced2
Jamal Maarad <j.maarad49@gmail.com>
2023-08-24 18:48:36 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Constructors */
+void main_cons(void) __attribute__((cons));
+/**
+ * main_cons - Constructor of function
+ * Return: Nothing
+ */
+void main_cons(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
