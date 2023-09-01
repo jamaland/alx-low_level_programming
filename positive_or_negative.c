1bcd4a6b040904cffaf3bbaa4a6d892c2209ce0f
Jamal Maarad <j.maarad49@gmail.com>
2023-08-31 18:51:25 -0700

    Create folder and README file

---
+++ positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's verify a number type'
+ * Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int n)
+{
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+}
