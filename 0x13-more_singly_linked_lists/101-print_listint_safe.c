b5d7d4ad3bf6074286ba7239d53cff6a85bfd524
jamaland <j.maarad49@gmail.com>
2023-09-27 16:16:39 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,30 @@
-#include "lists.h"
-#include <stdio.h>
+		new = malloc(sizeof(listp_t));
 
-/**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
-
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
-	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
+		if (new == NULL)
 			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
 		}
 
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		nodes++;
+		nnodes++;
 	}
-	head = NULL;
-	return (nodes);
+
+	free_listp(&hptr);
+	return (nnodes);
 }
