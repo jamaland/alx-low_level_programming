7b574127bf929116a2aeec65140a5a5529af9d11
jamaland <38596352+jamaland@users.noreply.github.com>
2023-09-28 12:45:12 +0100

    file io exercises

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * read_textfile - Program that reads a
+ * text file and prints it to the POSIX standard output.
+ * @filename: File name to read.
+ * @letters: size of letters on the given file.
+ * Return: 0 if Null.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fdo, fdr, fdw;
+	char *temp;
+
+	if (filename == NULL)
+		return (0);
+
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
+	{
+		free(temp);
+		return (0);
+	}
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdr = read(fdo, temp, letters);
+	temp[letters] = '\0';
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	if (fdw < 0)
+		return (0);
+	free(temp);
+	close(fdo);
+	return ((ssize_t)fdw);
+}
