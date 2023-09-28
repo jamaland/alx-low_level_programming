889244fd2ecc5ea250f73074a6310b6030702258
jamaland <38596352+jamaland@users.noreply.github.com>
2023-09-28 12:32:42 +0100

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * binary_to_uint - This Program converts binary number to an unsigned integer
+ * @b: Pointer
+ * Return: unsigned integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base, res, n;
+
+	base = 1;
+	res = 0;
+	n = 0;
+	if (b == NULL)
+		return (0);
+
+	while (b[n])
+		n++;
+
+	while (n)
+	{
+		if (b[n - 1] != '0' && b[n - 1] != '1')
+			return (0);
+
+		if (b[n - 1] == '1')
+			res += base;
+		base *= 2;
+		n--;
+	}
+	return (res);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - Function that sets the value of a bit to 0 at a given index.
+ * @n: unsigned long int.
+ * @index: bit index.
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
+}
